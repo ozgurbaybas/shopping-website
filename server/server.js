7333c00387529b82ccb5eeb32849f8322c63dcf1
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-04-07 15:27:37 +0300

    Load product from server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 import  express from "express";
+import  products from "./data/Products.js";
 
 const app = express()
+
+//Load Product From Server.
+
+app.get("/api/products", (req, res) => {
+    res.json(products)
+});
 
 app.get("/", (req, res) =>{
     res.send("WebApi is Running...");
