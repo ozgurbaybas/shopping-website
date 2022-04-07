be3c347322a7d7c27c08ff5bb285275784937b5f
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-04-07 15:10:38 +0300

    Products added to the server are complete

---
+++ server/data/Products.js
@@ -0,0 +1,114 @@
+const products = [
+  {
+    _id: "1",
+    name: "Red backpack",
+    image: "/images/1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 39,
+    countInStock: 4,
+    rating: 5,
+    numReviews: 5,
+  },
+  {
+    _id: "2",
+    name: "Blue long sleeve sweatshirt",
+    image: "/images/2.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 99,
+    countInStock: 15,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: "3",
+    name: "White sneakers",
+    image: "/images/3.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 125,
+    countInStock: 8,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Black backpack",
+    image: "/images/4.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 37,
+    countInStock: 14,
+    rating: 5,
+    numReviews: 8,
+  },
+  {
+    _id: "5",
+    name: "Men s shirt tie and vest set",
+    image: "/images/5.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 9,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: "6",
+    name: "Mens shirt tie and vest suit white",
+    image: "/images/6.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 150,
+    countInStock: 11,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "7",
+    name: "Women brown shoulder bag",
+    image: "/images/7.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 72,
+    countInStock: 5,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "8",
+    name: "Women black winter boots",
+    image: "/images/8.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 54,
+    countInStock: 6,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "9",
+    name: "Gray sneakers",
+    image: "/images/9.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 49,
+    countInStock: 18,
+    rating: 5,
+    numReviews: 6,
+  },
+  {
+    _id: "10",
+    name: "Mens golden yellow wristwatch",
+    image: "/images/10.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 60,
+    countInStock: 4,
+    rating: 4,
+    numReviews: 4,
+  },
+];
+
+export default products;
