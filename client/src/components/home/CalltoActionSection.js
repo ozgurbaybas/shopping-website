e4edca1f9597c292b96b2b884e1404350966110c
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-04-16 16:22:46 +0300

    The home component is created and the inner pages are created.

---
+++ client/src/components/home/CalltoActionSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CalltoActionSection() {
+  return (
+    <div>CalltoActionSection</div>
+  )
+}
+
+export default CalltoActionSection
---
+++ client/src/components/home/ShopSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShopSection() {
+  return (
+    <div>ShopSection</div>
+  )
+}
+
+export default ShopSection
